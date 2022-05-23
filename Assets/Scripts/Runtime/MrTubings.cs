8665e958592e328edf34bf6a78cac7d08e19ce39
Chris Helmich <kagekirin@gmail.com>
2022-05-23 14:19:40 +0900

    refactor: move vertex data generation into separate function

--- Assets/Scripts/Runtime/MrTubings.cs
+++ Assets/Scripts/Runtime/MrTubings.cs
@@ -83,146 +83,20 @@ namespace KageKirin.Mr_Tubings
                     math.mul(mWorldToLocal, math.float4(transform.parent.position, 1.0f)).xyz;
 
                 float3 axis = parentPositionInLocalSpace - localPositionInLocalSpace; //or basically parentPositionInLocalSpace
-                float axisLength = math.length(axis);
 
-                for (uint lateral = 0; lateral < lateralSubdivisions; lateral++)
-                {
-                    var nextLateral = lateral + 1; //< safe since we have lateralSubdivisions+1 values
-
-                    for (uint radial = 0; radial < radialSubdivisions; radial++)
-                    {
-                        var nextRadial = (radial + 1) % radialSubdivisions;
-
-                        float3[] quadVertices = new float3[4]
-                        {
-                            // note: xzy swizzle required, but float3(float2, float) makes this easier to read
-                            // note: then translate by lateral factor, here z * axis
-                            math.float3(
-                                radius * radialPositions[nextRadial],
-                                lateralPositions[lateral]
-                            ),
-                            math.float3(
-                                radius * radialPositions[radial],
-                                lateralPositions[lateral]
-                            ),
-                            math.float3(
-                                radius * radialPositions[radial],
-                                lateralPositions[nextLateral]
-                            ),
-                            math.float3(
-                                radius * radialPositions[nextRadial],
-                                lateralPositions[nextLateral]
-                            ),
-                        };
-
-                        float3[] quadNormals = new float3[4]
-                        {
-                            // note: xzy swizzle required as well
-                            // note: normalization required as well
-                            math.float3(radialPositions[nextRadial], 0.0f),
-                            math.float3(radialPositions[radial], 0.0f),
-                            math.float3(radialPositions[radial], 0.0f),
-                            math.float3(radialPositions[nextRadial], 0.0f),
-                        };
-
-                        float2[] quadTexcoords = new float2[4]
-                        {
-                            math.float2(
-                                (float)(radial + 1) / (float)radialSubdivisions,
-                                (float)lateral / (float)lateralSubdivisions
-                            ),
-                            math.float2(
-                                (float)(radial) / (float)radialSubdivisions,
-                                (float)lateral / (float)lateralSubdivisions
-                            ),
-                            math.float2(
-                                (float)(radial) / (float)radialSubdivisions,
-                                (float)(lateral + 1) / (float)lateralSubdivisions
-                            ),
-                            math.float2(
-                                (float)(radial + 1) / (float)radialSubdivisions,
-                                (float)(lateral + 1) / (float)lateralSubdivisions
-                            ),
-                        };
-
-                        BoneWeight[] quadWeights = new BoneWeight[4]
-                        {
-                            new BoneWeight()
-                            {
-                                boneIndex0 = tIdx,
-                                boneIndex1 = 0,
-                                boneIndex2 = 0,
-                                boneIndex3 = 0,
-                                weight0 = 1,
-                                weight1 = 0,
-                                weight2 = 0,
-                                weight3 = 0,
-                            },
-                            new BoneWeight()
-                            {
-                                boneIndex0 = tIdx,
-                                boneIndex1 = 0,
-                                boneIndex2 = 0,
-                                boneIndex3 = 0,
-                                weight0 = 1,
-                                weight1 = 0,
-                                weight2 = 0,
-                                weight3 = 0,
-                            },
-                            new BoneWeight()
-                            {
-                                boneIndex0 = tIdx,
-                                boneIndex1 = 0,
-                                boneIndex2 = 0,
-                                boneIndex3 = 0,
-                                weight0 = 1,
-                                weight1 = 0,
-                                weight2 = 0,
-                                weight3 = 0,
-                            },
-                            new BoneWeight()
-                            {
-                                boneIndex0 = tIdx,
-                                boneIndex1 = 0,
-                                boneIndex2 = 0,
-                                boneIndex3 = 0,
-                                weight0 = 1,
-                                weight1 = 0,
-                                weight2 = 0,
-                                weight3 = 0,
-                            },
-                        };
-
-                        int[] quadIndices = new int[4]
-                        {
-                            indices.Count,
-                            indices.Count + 1,
-                            indices.Count + 2,
-                            indices.Count + 3,
-                        };
-
-                        // offset, swizzle and transform quadVertices to rootSpace
-                        for (int i = 0; i < quadVertices.Length; i++)
-                        {
-                            ref float3 vertex = ref quadVertices[i];
-                            vertex = vertex.z * axis + math.float3(vertex.x, 0, vertex.y);
-                            vertex = math.mul(mLocalToRoot, math.float4(vertex, 1.0f)).xyz;
-                        }
-
-                        // swizzle and transform quadNormals to rootSpace
-                        for (int i = 0; i < quadNormals.Length; i++)
-                        {
-                            ref float3 normal = ref quadNormals[i];
-                            normal = math.mul(mLocalToRoot3x3, math.normalize(normal.xzy));
-                        }
-
-                        vertices.AddRange(quadVertices);
-                        normals.AddRange(quadNormals);
-                        uvs.AddRange(quadTexcoords);
-                        boneWeights.AddRange(quadWeights);
-                        indices.AddRange(quadIndices);
-                    }
-                }
+                GenerateVertexDataForBone(
+                    ref transform,
+                    tIdx,
+                    axis,
+                    mWorldToRootSpace,
+                    radialPositions,
+                    lateralPositions,
+                    vertices,
+                    normals,
+                    uvs,
+                    boneWeights,
+                    indices
+                );
             }
 
             // create new mesh
@@ -260,6 +134,164 @@ namespace KageKirin.Mr_Tubings
             // set skin renderer data
             skinnedMeshRenderer.bones = transforms;
             skinnedMeshRenderer.rootBone = rootTransform;
+        }
+
+        void GenerateVertexDataForBone(
+            ref Transform transform,
+            in int transformIndex,
+            in float3 axis,
+            in float4x4 mWorldToRootSpace,
+            in float2[] radialPositions,
+            in float[] lateralPositions,
+            List<float3> vertices,
+            List<float3> normals,
+            List<float2> uvs,
+            List<BoneWeight> boneWeights,
+            List<int> indices
+        )
+        {
+            float4x4 mWorldToLocal = transform.worldToLocalMatrix;
+            float4x4 mLocalToWorld = transform.localToWorldMatrix;
+            float4x4 mLocalToRoot = math.mul(mWorldToRootSpace, mLocalToWorld);
+            float3x3 mLocalToRoot3x3 = math.float3x3(mLocalToRoot);
+
+            float axisLength = math.length(axis);
+
+            for (uint lateral = 0; lateral < lateralSubdivisions; lateral++)
+            {
+                var nextLateral = lateral + 1; //< safe since we have lateralSubdivisions+1 values
+
+                for (uint radial = 0; radial < radialSubdivisions; radial++)
+                {
+                    var nextRadial = (radial + 1) % radialSubdivisions;
+
+                    float3[] quadVertices = new float3[4]
+                    {
+                        // note: xzy swizzle required, but float3(float2, float) makes this easier to read
+                        // note: then translate by lateral factor, here z * axis
+                        math.float3(
+                            radius * radialPositions[nextRadial],
+                            lateralPositions[lateral]
+                        ),
+                        math.float3(radius * radialPositions[radial], lateralPositions[lateral]),
+                        math.float3(
+                            radius * radialPositions[radial],
+                            lateralPositions[nextLateral]
+                        ),
+                        math.float3(
+                            radius * radialPositions[nextRadial],
+                            lateralPositions[nextLateral]
+                        ),
+                    };
+
+                    float3[] quadNormals = new float3[4]
+                    {
+                        // note: xzy swizzle required as well
+                        // note: normalization required as well
+                        math.float3(radialPositions[nextRadial], 0.0f),
+                        math.float3(radialPositions[radial], 0.0f),
+                        math.float3(radialPositions[radial], 0.0f),
+                        math.float3(radialPositions[nextRadial], 0.0f),
+                    };
+
+                    float2[] quadTexcoords = new float2[4]
+                    {
+                        math.float2(
+                            (float)(radial + 1) / (float)radialSubdivisions,
+                            (float)lateral / (float)lateralSubdivisions
+                        ),
+                        math.float2(
+                            (float)(radial) / (float)radialSubdivisions,
+                            (float)lateral / (float)lateralSubdivisions
+                        ),
+                        math.float2(
+                            (float)(radial) / (float)radialSubdivisions,
+                            (float)(lateral + 1) / (float)lateralSubdivisions
+                        ),
+                        math.float2(
+                            (float)(radial + 1) / (float)radialSubdivisions,
+                            (float)(lateral + 1) / (float)lateralSubdivisions
+                        ),
+                    };
+
+                    BoneWeight[] quadWeights = new BoneWeight[4]
+                    {
+                        new BoneWeight()
+                        {
+                            boneIndex0 = transformIndex,
+                            boneIndex1 = 0,
+                            boneIndex2 = 0,
+                            boneIndex3 = 0,
+                            weight0 = 1,
+                            weight1 = 0,
+                            weight2 = 0,
+                            weight3 = 0,
+                        },
+                        new BoneWeight()
+                        {
+                            boneIndex0 = transformIndex,
+                            boneIndex1 = 0,
+                            boneIndex2 = 0,
+                            boneIndex3 = 0,
+                            weight0 = 1,
+                            weight1 = 0,
+                            weight2 = 0,
+                            weight3 = 0,
+                        },
+                        new BoneWeight()
+                        {
+                            boneIndex0 = transformIndex,
+                            boneIndex1 = 0,
+                            boneIndex2 = 0,
+                            boneIndex3 = 0,
+                            weight0 = 1,
+                            weight1 = 0,
+                            weight2 = 0,
+                            weight3 = 0,
+                        },
+                        new BoneWeight()
+                        {
+                            boneIndex0 = transformIndex,
+                            boneIndex1 = 0,
+                            boneIndex2 = 0,
+                            boneIndex3 = 0,
+                            weight0 = 1,
+                            weight1 = 0,
+                            weight2 = 0,
+                            weight3 = 0,
+                        },
+                    };
+
+                    int[] quadIndices = new int[4]
+                    {
+                        indices.Count,
+                        indices.Count + 1,
+                        indices.Count + 2,
+                        indices.Count + 3,
+                    };
+
+                    // offset, swizzle and transform quadVertices to rootSpace
+                    for (int i = 0; i < quadVertices.Length; i++)
+                    {
+                        ref float3 vertex = ref quadVertices[i];
+                        vertex = vertex.z * axis + math.float3(vertex.x, 0, vertex.y);
+                        vertex = math.mul(mLocalToRoot, math.float4(vertex, 1.0f)).xyz;
+                    }
+
+                    // swizzle and transform quadNormals to rootSpace
+                    for (int i = 0; i < quadNormals.Length; i++)
+                    {
+                        ref float3 normal = ref quadNormals[i];
+                        normal = math.mul(mLocalToRoot3x3, math.normalize(normal.xzy));
+                    }
+
+                    vertices.AddRange(quadVertices);
+                    normals.AddRange(quadNormals);
+                    uvs.AddRange(quadTexcoords);
+                    boneWeights.AddRange(quadWeights);
+                    indices.AddRange(quadIndices);
+                }
+            }
         }
     }
 }
